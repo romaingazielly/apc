b13e8e6aceb204d09b5d9a5ab10a6dc8190f763a
Romain <romain.gazielly@gmail.com>
2015-11-26 13:00:56 +0100

    Enseignements - single

--- wp-content/themes/apc/page-enseignement.php
+++ wp-content/themes/apc/page-enseignement.php
@@ -3,8 +3,6 @@
 	<?php
 
 	$post = get_page_by_path('enseignement',OBJECT,'page');
-	$content = $post->post_content;
-	$i = 0;
 
 	?>
 	<main role="main">
@@ -12,7 +10,7 @@
 	<section class="contact-container page-format sizer">
 		<section class="enseignements">
 			<?php if (have_posts()): while (have_posts()) : the_post(); ?><div class="year" style="background:url('<?php echo $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>'); background-size:cover;">
-				<a href="">
+				<a href="<?php the_permalink(); ?>">
 					<aside><?php echo get_the_title() ?></aside>
 					<img src="<?php echo $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>" alt="<?php echo get_the_title() ?>">
 				</a>
@@ -21,7 +19,7 @@
 	 --><aside class="map-infos">
 	 		<div>
 				<h1>Enseignement</h1>
-				<p><?php echo $content ?></p>
+				<p><?php echo get_post_meta($post->ID, 'description', true) ?></p>
 			</div>
 		</aside>
 	</section>
